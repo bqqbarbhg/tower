d718dc2d488ec88b4bbf8c8087bb72d58886d05a
bqqbarbhg <samuli.1995@hotmail.com>
2018-01-30 00:01:28 +0200

    Separate keyframe reduction from rotation

--- src/authoring/processing/AnimationOptimization.scala
+++ src/authoring/processing/AnimationOptimization.scala
@@ -8,41 +8,33 @@ import scala.collection.mutable.ArrayBuffer
 object AnimationOptimization {
 
   /**
-    * Removes rotation keyframes that could be replaced by linear interpolation (within an error margin).
+    * Removes intervals of keyframes that could be replaced by interpolating between their endpoints.
+    *
+    * @param numKeyframes Total number of original keyframes
+    * @param canReplaceFn Function that returns true if the interval (begin, end) can be reduced into
+    *                     those two endpoints. Guarantee: `begin + 1 < end`
+    * @return Array keyframe indices to _keep_ from the originals
     *
     * Complexity: O(n log n)
     */
-  def reduceRotationKeyframes(timeline: Timeline, maximumError: Double): Timeline = {
-
+  private def reduceKeyframes(numKeyframes: Int, canReplaceFn: (Int, Int) => Boolean): ArrayBuffer[Int] = {
     // Explicitly handle degenerate cases
-    if (timeline.rot.length <= 2) return timeline
-
-    /** Returns whether a linear interpolation between the begin (inclusive) and end (inclusive) indices represents
-      * the whole interval within the error margin. */
-    def canRangeBeReplaced(begin: Int, end: Int): Boolean = {
-
-      // Explicitly handle degenerate cases
-      if (begin + 1 >= end) return true
-
-      val a: FrameRot = timeline.rot(begin)
-      val b: FrameRot = timeline.rot(end)
-
-      // Linearly scan through all the midpoints and check that the interpolated value matches the actual values
-      for (index <- (begin + 1) to (end - 1)) {
-        val mid = timeline.rot(index)
-        val t = (mid.time - a.time) / (b.time - a.time)
-        val quat = Quaternion.lerp(a.rot, b.rot, t).normalize
-        if ((mid.rot - quat).length > maximumError) return false
-      }
-
-      true
+    numKeyframes match {
+      case 0 => return ArrayBuffer[Int]()
+      case 1 => return ArrayBuffer[Int](0)
+      case 2 => return ArrayBuffer[Int](0, 1)
+      case _ => // Do the actual reduction
     }
 
-    val newKeyframes = new ArrayBuffer[FrameRot]()
-    val numKeyframes = timeline.rot.length
+    // Wrap the user callback handing degenerate cases gracefully
+    def canRangeBeReplaced(begin: Int, end: Int): Boolean = {
+      if (begin + 1 >= end) return true
+      canReplaceFn(begin, end)
+    }
 
-    // Always insert the first keyframe
-    newKeyframes += timeline.rot(0)
+    // Always keep the first keyframe
+    val keyframesToKeep = new ArrayBuffer[Int]()
+    keyframesToKeep += 0
 
     // Scan through all the keyframes replacing all possible intervals
     var currentIndex = 0
@@ -73,10 +65,34 @@ object AnimationOptimization {
       assert(currentIndex + 1 < numKeyframes)    // Due to the `while` condition
       assert(lastAcceptableIndex > currentIndex) // Due to above and `canRangeBeReplaced` explicit degenerate case
 
-      newKeyframes += timeline.rot(lastAcceptableIndex)
+      keyframesToKeep += lastAcceptableIndex
       currentIndex = lastAcceptableIndex
     }
 
+    keyframesToKeep
+  }
+
+  /**
+    * Removes rotation keyframes that could be replaced by linear interpolation (within an error margin).
+    */
+  def reduceRotationKeyframes(timeline: Timeline, maximumError: Double): Timeline = {
+
+    def canRangeBeReplaced(begin: Int, end: Int): Boolean = {
+      val a: FrameRot = timeline.rot(begin)
+      val b: FrameRot = timeline.rot(end)
+
+      for (index <- (begin + 1) to (end - 1)) {
+        val mid = timeline.rot(index)
+        val t = (mid.time - a.time) / (b.time - a.time)
+        val quat = Quaternion.lerp(a.rot, b.rot, t).normalize
+        if ((mid.rot - quat).length > maximumError) return false
+      }
+
+      true
+    }
+
+    val keepFrames = reduceKeyframes(timeline.rot.length, canRangeBeReplaced)
+    val newKeyframes = keepFrames.map(timeline.rot)
     timeline.copy(rot = newKeyframes.toArray)
   }
 
