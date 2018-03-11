a83928bb8a4c1d8e4914480631309fc173c2400f
bqqbarbhg <samuli.1995@hotmail.com>
2018-03-11 13:45:56 +0200

    Add aliases and multi-arguments to ArgumentParser

--- shared/src/util/ArgumentParser.scala
+++ shared/src/util/ArgumentParser.scala
@@ -8,14 +8,19 @@ object ArgumentParser {
   /**
     * Represents parsed argument values.
     *
-    * Example: hello.txt --standalone -i --num=10 -o thing.out other.txt
-    * called with `parse(args, implicitArgumentFlags=Vector("-o"))`
+    * Example: hello.txt --standalone -i --num=10 -o thing.out other.txt -L=lib -L=other
+    * called with `parse(args, implicitArgumentFlags=Vector("o"), multiArgumentFlags=Vector("L"))`
     *
     * @param positional Positional non-flag arguments, in example: [hello.txt, other.txt]
     * @param keywords Flags with values, in example: [num -> 10, o -> thing.out]
+    * @param multiKeywords Flags with multiple values, in example: [L -> [lib, other]]
     * @param flags Set of enabled flags, in example: [standalone, i]
     */
-  case class Args(positional: Vector[String], keywords: Map[String, String], flags: Set[String]) {
+  case class Args(
+    positional: Vector[String],
+    keywords: Map[String, String],
+    multiKeywords: Map[String, Vector[String]],
+    flags: Set[String]) {
 
     /** Returns whether a flag is set */
     def flag(flag: String): Boolean = flags.contains(flag)
@@ -29,39 +34,65 @@ object ArgumentParser {
     * @param implicitArgumentFlags Determines flags that should be treated as taking an value.
     *                              For example if set to `"o"` then "-o foo.txt" would be
     *                              parsed as "-o=foo.txt"
+    * @param multiArgumentFlags Determines flags that can have multiple values at the same
+    *                           time. For example if `"L"` then "-L=first -L=second" will
+    *                           result in `L = [first, second]`. Otherwise only the last
+    *                           argument is taken.
+    * @param aliases Aliases for flags. For example if `"F" -> "flag"` then "-F" will be
+    *                parsed as "-flag"
     */
-  def parse(args: Iterable[String], implicitArgumentFlags: Iterable[String] = Vector()): Args = {
+  def parse(args: Iterable[String],
+            implicitArgumentFlags: Iterable[String] = Vector(),
+            multiArgumentFlags: Iterable[String] = Vector(),
+            aliases: Map[String, String] = Map[String, String]()): Args = {
     val implicitArgs = implicitArgumentFlags.toSet
     val pos = new VectorBuilder[String]
     val kw = new mutable.HashMap[String, String]()
-    val flag = new mutable.HashSet[String]()
+    val multiKw = new mutable.HashMap[String, VectorBuilder[String]]()
+    val flags = new mutable.HashSet[String]()
+
+    // Create empty containers for all the multi-keywords
+    for (flag <- multiArgumentFlags) {
+      multiKw(flag) = new VectorBuilder[String]
+    }
+
+    def setKw(key: String, value: String): Unit = {
+      val maybeMulti = multiKw.get(key)
+      maybeMulti match {
+        case Some(multi) => multi += value
+        case None => kw(key) = value
+      }
+    }
 
     var implicitKeywordActivated = ""
     for (arg <- args.filter(_.nonEmpty)) {
       // Implicit keywords take priority
       if (implicitKeywordActivated.nonEmpty) {
-        kw(implicitKeywordActivated) = arg
+        setKw(implicitKeywordActivated, arg)
         implicitKeywordActivated = ""
       } else if (arg.head == '-') {
         val tail = arg.dropWhile(_ == '-')
         val eqIx = tail.indexOf('=')
+        val rawFlag = if (eqIx >= 0) tail.take(eqIx) else tail
+        val flag = aliases.get(rawFlag).getOrElse(rawFlag)
+
         if (eqIx >= 0) {
           // -foo=thing
-          val key = tail.take(eqIx)
           val value = tail.drop(eqIx + 1)
-          kw(key) = value
-        } else if (implicitArgs.contains(tail)) {
+          setKw(flag, value)
+        } else if (implicitArgs.contains(flag)) {
           // -o thing
-          implicitKeywordActivated = tail
+          implicitKeywordActivated = flag
         } else {
           // --flag
-          flag += tail
+          flags += flag
         }
       } else {
         pos += arg
       }
     }
 
-    Args(pos.result, kw.toMap, flag.toSet)
+    val multi = multiKw.mapValues(_.result).toMap
+    Args(pos.result, kw.toMap, multi, flags.toSet)
   }
 }
--- test/src/util/ArgumentParserTest.scala
+++ test/src/util/ArgumentParserTest.scala
@@ -21,6 +21,7 @@ class ArgumentParserTest extends FlatSpec with Matchers {
     assert(arg.positional(2) === "yes")
     assert(arg.positional(3) === "10")
     assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.isEmpty)
     assert(arg.flags.isEmpty)
   }
 
@@ -32,6 +33,7 @@ class ArgumentParserTest extends FlatSpec with Matchers {
     assert(arg.keywords.size === 2)
     assert(arg.keywords("key") === "val")
     assert(arg.keywords("o") === "file")
+    assert(arg.multiKeywords.isEmpty)
     assert(arg.flags.isEmpty)
   }
 
@@ -41,6 +43,7 @@ class ArgumentParserTest extends FlatSpec with Matchers {
 
     assert(arg.positional.isEmpty)
     assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.isEmpty)
     assert(arg.flags.size === 2)
     assert(arg.flags.contains("f"))
     assert(arg.flags.contains("force"))
@@ -54,6 +57,7 @@ class ArgumentParserTest extends FlatSpec with Matchers {
     assert(arg.positional(0) === "pos")
     assert(arg.keywords.size === 1)
     assert(arg.keywords("key") === "val")
+    assert(arg.multiKeywords.isEmpty)
     assert(arg.flags.size === 1)
     assert(arg.flags.contains("flag"))
   }
@@ -64,6 +68,7 @@ class ArgumentParserTest extends FlatSpec with Matchers {
 
     assert(arg.positional.isEmpty)
     assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.isEmpty)
     assert(arg.flags.size === 1)
     assert(arg.flags.contains("extra--dashes---"))
   }
@@ -76,6 +81,7 @@ class ArgumentParserTest extends FlatSpec with Matchers {
     assert(arg.keywords.size === 2)
     assert(arg.keywords("o") === "foo")
     assert(arg.keywords("key") === "val")
+    assert(arg.multiKeywords.isEmpty)
     assert(arg.flags.isEmpty)
   }
 
@@ -87,6 +93,7 @@ class ArgumentParserTest extends FlatSpec with Matchers {
     assert(arg.keywords.size === 2)
     assert(arg.keywords("o") === "foo")
     assert(arg.keywords("key") === "val")
+    assert(arg.multiKeywords.isEmpty)
     assert(arg.flags.isEmpty)
   }
 
@@ -97,7 +104,126 @@ class ArgumentParserTest extends FlatSpec with Matchers {
     assert(arg.positional.isEmpty)
     assert(arg.keywords.size === 1)
     assert(arg.keywords("o") === "foo")
+    assert(arg.multiKeywords.isEmpty)
     assert(arg.flags.isEmpty)
   }
 
+  "multiArgumentFlags" should "parse multi keyword arguments" in {
+    val args = "-L=first -L=second".split(' ')
+    val arg = ArgumentParser.parse(args,
+      multiArgumentFlags = Vector("L"))
+
+    assert(arg.positional.isEmpty)
+    assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.size === 1)
+    assert(arg.multiKeywords("L").size === 2)
+    assert(arg.multiKeywords("L")(0) === "first")
+    assert(arg.multiKeywords("L")(1) === "second")
+    assert(arg.flags.isEmpty)
+  }
+
+  it should "work with implicitArgumentFlags" in {
+    val args = "-L first -L second".split(' ')
+    val arg = ArgumentParser.parse(args,
+      implicitArgumentFlags = Vector("L"),
+      multiArgumentFlags = Vector("L"))
+
+    assert(arg.positional.isEmpty)
+    assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.size === 1)
+    assert(arg.multiKeywords("L").size === 2)
+    assert(arg.multiKeywords("L")(0) === "first")
+    assert(arg.multiKeywords("L")(1) === "second")
+    assert(arg.flags.isEmpty)
+  }
+
+  "aliases" should "map argument names with aliases" in {
+    val args = "-F".split(' ')
+    val arg = ArgumentParser.parse(args, aliases = Map("F" -> "flag"))
+
+    assert(arg.positional.isEmpty)
+    assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.isEmpty)
+    assert(arg.flags.size === 1)
+    assert(arg.flags.contains("flag"))
+  }
+
+  it should "not mess with the original name" in {
+    val args = "-flag".split(' ')
+    val arg = ArgumentParser.parse(args, aliases = Map("F" -> "flag"))
+
+    assert(arg.positional.isEmpty)
+    assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.isEmpty)
+    assert(arg.flags.size === 1)
+    assert(arg.flags.contains("flag"))
+  }
+
+  it should "not affect positional arguments" in {
+    val args = "F".split(' ')
+    val arg = ArgumentParser.parse(args, aliases = Map("F" -> "flag"))
+
+    assert(arg.positional.size === 1)
+    assert(arg.positional(0) === "F")
+    assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.isEmpty)
+    assert(arg.flags.isEmpty)
+  }
+
+  it should "work with keyword arguments" in {
+    val args = "-F=value".split(' ')
+    val arg = ArgumentParser.parse(args, aliases = Map("F" -> "flag"))
+
+    assert(arg.positional.isEmpty)
+    assert(arg.keywords.size === 1)
+    assert(arg.keywords("flag") === "value")
+    assert(arg.multiKeywords.isEmpty)
+    assert(arg.flags.isEmpty)
+  }
+
+  it should "work with implicit keyword arguments" in {
+    val args = "-F value".split(' ')
+    val arg = ArgumentParser.parse(args,
+      implicitArgumentFlags = Vector("flag"),
+      aliases = Map("F" -> "flag"))
+
+    assert(arg.positional.isEmpty)
+    assert(arg.keywords.size === 1)
+    assert(arg.keywords("flag") === "value")
+    assert(arg.multiKeywords.isEmpty)
+    assert(arg.flags.isEmpty)
+  }
+
+  it should "work with multi-keyword arguments" in {
+    val args = "-F=value -F=other -flag=old".split(' ')
+    val arg = ArgumentParser.parse(args,
+      multiArgumentFlags = Vector("flag"),
+      aliases = Map("F" -> "flag"))
+
+    assert(arg.positional.isEmpty)
+    assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.size === 1)
+    assert(arg.multiKeywords("flag").size === 3)
+    assert(arg.multiKeywords("flag")(0) === "value")
+    assert(arg.multiKeywords("flag")(1) === "other")
+    assert(arg.multiKeywords("flag")(2) === "old")
+    assert(arg.flags.isEmpty)
+  }
+
+  it should "work with implicit multi-keyword arguments" in {
+    val args = "-F value -F=other -flag=old".split(' ')
+    val arg = ArgumentParser.parse(args,
+      implicitArgumentFlags = Vector("flag"),
+      multiArgumentFlags = Vector("flag"),
+      aliases = Map("F" -> "flag"))
+
+    assert(arg.positional.isEmpty)
+    assert(arg.keywords.isEmpty)
+    assert(arg.multiKeywords.size === 1)
+    assert(arg.multiKeywords("flag").size === 3)
+    assert(arg.multiKeywords("flag")(0) === "value")
+    assert(arg.multiKeywords("flag")(1) === "other")
+    assert(arg.multiKeywords("flag")(2) === "old")
+    assert(arg.flags.isEmpty)
+  }
 }
