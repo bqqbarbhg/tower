1f6d482a3e83565835b0fd6a75b5d9e9b94e2a04
bqqbarbhg <samuli.1995@hotmail.com>
2018-04-16 00:15:19 +0300

    Documentation for OpenEXR

--- engine/src/io/format/OpenExr.scala
+++ engine/src/io/format/OpenExr.scala
@@ -16,6 +16,7 @@ object OpenExr {
 
     val tempBytes = Memory.alloc(1024)
 
+    /** Write zero terminated string `str` to `buf` */
     def puts(buf: ByteBuffer, str: String): Unit = {
       val chars = str.toCharArray
       for (c <- chars) {
@@ -24,65 +25,91 @@ object OpenExr {
       buf.put(0.toByte)
     }
 
+    /**
+      * Write OpenEXR header attribute. Attributes consist of name, type,
+      * value size, and type-specific value data.
+      */
     def putAttrib(name: String, typ: String, write: ByteBuffer => Unit): Unit = {
+
+      // Write the attribute into a temporary buffer
       tempBytes.position(0)
       write(tempBytes)
-      val num = tempBytes.position
+      val valueSize = tempBytes.position
       tempBytes.position(0)
       val slice = tempBytes.sliceEx
       slice.limit(num)
-      puts(dst, name)
-      puts(dst, typ)
-      dst.putInt(num)
-      dst.put(slice)
+
+      puts(dst, name)       // Attribute name
+      puts(dst, typ)        // Attribute type
+      dst.putInt(valueSize) // Value size
+      dst.put(slice)        // Value data (from temporary buffer)
     }
 
+    // -- Components One and Two: Magic Number and Version Field
+
+    // Magic Number
+    // The magic number, of type int, is always 20000630 (decimal).
     dst.putInt(20000630)
 
+    // Version Field
+    // The version field, of type int, is the four-byte group following the magic number,
+    // and it is treated as two separate bit fields.
+    // The 8 least significant bits, they contain the file format version number.
+    // The current OpenEXR version number is version 2.
+    // The 24 most significant bits, these are treated as a set of boolean flags.
+    // - No flags are necessary for a simple non-multipart non-chunked image
     val version = 2
     var flags = 0x0
     dst.putInt(version | flags)
 
+    // -- Component Three: Header
+
     putAttrib("channels", "chlist", b => {
       for (ch <- Array("B", "G", "R")) {
-        puts(b, ch)
-        b.putInt(2)
-        b.put(1.toByte)
-        b.put(0.toByte)
-        b.put(0.toByte)
-        b.put(0.toByte)
-        b.putInt(1)
-        b.putInt(1)
+        puts(b, ch)     // Channel name
+        b.putInt(2)     // Format: FLOAT
+        b.put(1.toByte) // Linear: TRUE
+        b.put(0.toByte) // Reserved
+        b.put(0.toByte) // Reserved
+        b.put(0.toByte) // Reserved
+        b.putInt(1)     // Subsamples X: 1
+        b.putInt(1)     // Subsamples Y: 1
       }
 
+      // Null-terminator for channels
       b.put(0.toByte)
     })
 
     putAttrib("compression", "compression", b => {
-      b.put(0.toByte)
+      b.put(0.toByte) // NO_COMPRESSION
     })
 
     putAttrib("dataWindow", "box2i", b => {
-      b.putInt(0)
-      b.putInt(0)
-      b.putInt(width - 1)
-      b.putInt(height - 1)
+      b.putInt(0)          // minX (inclusive)
+      b.putInt(0)          // minY (inclusive)
+      b.putInt(width - 1)  // maxX (inclusive)
+      b.putInt(height - 1) // maxY (inclusive)
     })
 
     putAttrib("displayWindow", "box2i", b => {
-      b.putInt(0)
-      b.putInt(0)
-      b.putInt(width - 1)
-      b.putInt(height - 1)
+      b.putInt(0)          // minX (inclusive)
+      b.putInt(0)          // minY (inclusive)
+      b.putInt(width - 1)  // maxX (inclusive)
+      b.putInt(height - 1) // maxY (inclusive)
     })
 
     putAttrib("lineOrder", "lineOrder", b => {
-      b.put(0.toByte)
+      b.put(0.toByte)      // INCREASING_Y
     })
 
     putAttrib("pixelAspectRatio", "float", b => {
       b.putFloat(1.0f)
     })
+
+    // Programs that deal with images as purely two-dimensional objects
+    // may not be able to generate a description of a perspective
+    // projection. Those programs should set screenWindowWidth to 1, and
+    // screenWindowCenter to (0, 0).
 
     putAttrib("screenWindowCenter", "v2f", b => {
       b.putFloat(0.0f)
@@ -93,7 +120,15 @@ object OpenExr {
       b.putFloat(1.0f)
     })
 
+    // Null-terminator for header attributes
     dst.put(0.toByte)
+
+    // -- Component Four: Offset Tables
+
+    // An offset table allows random access to pixel data chunks. An
+    // offset table is a sequence of offsets, with one offset per chunk.
+    // Each offset (of type unsigned long) indicates the distance, in
+    // bytes, between the start of the file and the start of the chunk.
 
     val OffsetSize = 8
     val LineSize = width * 3 * 4
@@ -104,9 +139,21 @@ object OpenExr {
       dst.putLong(dataBegin + i * ChunkSize)
     }
 
+    // -- Component Five: Pixel data
+
     for (y <- 0 until height) {
-      dst.putInt(y)
-      dst.putInt(LineSize)
+
+      // Regular scan line image block layout
+
+      dst.putInt(y)        // Y-coordinate
+      dst.putInt(LineSize) // Pixel data size
+
+      // Pixel data
+      // Within the pixel data, scan lines are stored top to bottom.
+      // Each scan line is contiguous, and within a scan line the data
+      // for each channel are contiguous. Channels are stored in
+      // alphabetical order, according to channel names. Within a channel,
+      // pixels are stored left to right.
 
       val base = (height - y - 1) * scanLinePitch
 
